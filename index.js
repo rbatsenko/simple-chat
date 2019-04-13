84e508e5a1bc90af3ebb621bc86050c88f1c82ea
Roman Batsenko <rbatsenko@gmail.com>
2019-04-13 08:21:26 +0200

    simple chat

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+io.on('connection', (socket) => {
+  console.log('An user connected');
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
 http.listen(3000, () => {
   console.log('Listening on *:3000');
 });
