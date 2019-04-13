0605622e3fb6caf679b8a5e229ed067ebe39aeab
Roman Batsenko <rbatsenko@gmail.com>
2019-04-13 23:37:08 +0200

    fix view

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('An user connected');
+  console.log('User connected');
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
